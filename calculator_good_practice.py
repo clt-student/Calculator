584c7762b4d8e70729465e14e560a43ee02ddc7d
clt-student <wadams18@charlotte.edu>
2025-06-24 17:43:19 -0400

    feat: implement subtract method for Calculator class

--- calculator_good_practice.py
+++ calculator_good_practice.py
@@ -4,3 +4,7 @@ class Calculator:
     def add(self, a, b):
         """ Returns the sum of two numbers"""
         return a + b
+    
+    def subtract(self, a, b):
+        """ Returns the difference of two numbers"""
+        return a - b
