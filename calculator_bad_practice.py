c30bb841156eba098c3be97e22b74a9c9d340ccb
clt-student <50535899+wadams-dev@users.noreply.github.com>
2025-06-24 21:07:42 -0400

    feat: implement subtraction operation and else condition for invalid operation

--- calculator_bad_practice.py
+++ calculator_bad_practice.py
@@ -5,8 +5,14 @@ c = input("Operation: ")
 if c == "+":
     print("Sum is: " + str(a+b))
 
+elif c == "-":
+    print("Difference is: " + str(a-b))
+
 elif c == "*":
     print("Product is: " + str(a * b))
 
 elif c == "/":
     print("Division is: " + str(a/b))
+    
+else:
+    print("Invalid operation. Please use +, -, *, or /.")
